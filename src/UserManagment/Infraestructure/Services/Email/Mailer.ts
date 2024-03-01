0ac01c768595874187da57e142c976576fa15ccd
Fernando Daniel Pérez Pérez <213524@ids.upchiapas.edu.mx>
2024-02-29 21:01:22 -0600

    first commit

---
+++ src/UserManagment/Infraestructure/Services/Email/Mailer.ts
@@ -0,0 +1,20 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: '213497@ids.upchiapas.edu.mx',
+        pass: 'you password'
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+})
+
+transporter.verify().then(() => {
+    console.log('Ready for sends emails');
+})
+
+export { transporter }
